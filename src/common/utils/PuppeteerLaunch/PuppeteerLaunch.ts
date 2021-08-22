ba05f6f46c908247eb8e7e6d93918b330a538548
Hállan da Silva Costa <hallan.costa1@hotmail.com>
2021-08-22 18:18:12 -0300

    remove executablePath
    Signed-off-by: Hállan da Silva Costa <hallan.costa1@hotmail.com>

--- src/common/utils/PuppeteerLaunch/PuppeteerLaunch.ts
+++ src/common/utils/PuppeteerLaunch/PuppeteerLaunch.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 export const launchConfig = {
   // executablePath: path.join("/", "mnt", "c", "Program Files (x86)", "Microsoft", "Edge", "Application", "msedge.exe"),
-  executablePath: path.join("/", "mnt", "c", "chrome-win", "chrome.exe"),
+  // executablePath: path.join("/", "mnt", "c", "chrome-win", "chrome.exe"),
   headless: true,
   args: ["--no-sandbox"]
 }
